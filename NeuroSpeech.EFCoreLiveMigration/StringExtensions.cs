c7b07195b8ecc5dfe204721561d3788a6901eb15
Akash Kava <39438041+ackava@users.noreply.github.com>
2019-02-07 13:29:21 +0530

    vb

--- NeuroSpeech.EFCoreLiveMigration/StringExtensions.cs
+++ NeuroSpeech.EFCoreLiveMigration/StringExtensions.cs
@@ -8,7 +8,7 @@ using System.Text;
 
 namespace NeuroSpeech.EFCoreLiveMigration
 {
-    public static class StringExtensions
+    internal static class StringExtensions
     {
 
         public static bool ContainsIgnoreCase(this string text, string test) {
